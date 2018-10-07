9bfa0a01091fe4595fd2d5795cef245b2fa93964
psatler <pablosatler@gmail.com>
2018-10-07 16:36:22 -0300

    feat: add first type to schema

---
+++ server/schema/schema.js
@@ -0,0 +1,12 @@
+//the schema is going the describe the data, the graph, the types and relations among the types
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
