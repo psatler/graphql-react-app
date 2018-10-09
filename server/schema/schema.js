a376514e3b933acf92e131efae52c9cc7967b33b
psatler <pablosatler@gmail.com>
2018-10-08 21:26:57 -0300

    feat: add checks for non-null entries in gql queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,7 +7,8 @@ const {
   GraphQLSchema,
   GraphQLID,
   GraphQLInt,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull //adding this so we can specify to not accept non null values
 } = graphql;
 
 //importing the models from mongoose/mongodb
@@ -119,8 +120,8 @@ const Mutation = new GraphQLObjectType({
       //mutation to add an author
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) }
       },
       resolve(parent, args) {
         //using the model constructor imported above
@@ -135,9 +136,9 @@ const Mutation = new GraphQLObjectType({
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        authorId: { type: GraphQLID }
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) }
       },
       resolve(parent, args) {
         let book = new Book({
