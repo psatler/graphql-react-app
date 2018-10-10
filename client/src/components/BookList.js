efd282573cfaa86fc60c82437f260bbad570f6d4
psatler <pablosatler@gmail.com>
2018-10-09 22:59:04 -0300

    feat: create graphql queries from react

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
+import { gql } from "apollo-boost"; //importing gql so we can parse/construct graphql queries into JS
+import { graphql } from "react-apollo"; //react apollo bindings - it is bound using currying (at the export default below)
+
+//constructing the graphql query
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 class BookList extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>
         <h1>Testing</h1>
@@ -13,4 +26,5 @@ class BookList extends Component {
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList); //using the bindings from react apollo and the constructed query above
+//we have access to the data queried from gql server using the component's props
